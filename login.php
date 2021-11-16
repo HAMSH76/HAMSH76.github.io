3c230c26fbd8884fcf8af7485bf5c64fde2a067d
HAMSH76 <45772269+HAMSH76@users.noreply.github.com>
2021-11-17 08:04:39 +1000

    Add files via upload

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+<title>login page</title>
+	<link rel="stylesheet" href="style2.css">
+</head>
+<body>
+	<form action="login2.php" method="post">
+  <div class="imgcontainer">
+    <img src="logo.png" width="90" height="90">
+  </div>
+
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit">Login</button>
+  </div>
+
+  </div>
+</form>
+	</form>
+</body>
+</html>
---
+++ login2.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> sucessfull login attempt</title>
+	<link rel="stylesheet" href="style3.css">
+</head>
+<body>
+<p>login attempt potentially sucessfull</p>
+</body>
+</html>
